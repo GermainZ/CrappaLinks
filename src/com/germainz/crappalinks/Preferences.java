efec431ed92bea0c2b3394bb284b5956675a5465
GermainZ <germanosz@gmail.com>
2014-01-30 02:02:46 +0200

    Added preference to hide icon from launcher

--- src/com/germainz/crappalinks/Preferences.java
+++ src/com/germainz/crappalinks/Preferences.java
@@ -1,7 +1,10 @@
 package com.germainz.crappalinks;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
 public class Preferences extends Activity {
@@ -10,8 +13,7 @@ public class Preferences extends Activity {
         super.onCreate(savedInstanceState);
 
         if (savedInstanceState == null)
-            getFragmentManager().beginTransaction().replace(android.R.id.content,
-                    new PrefsFragment()).commit();
+            getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefsFragment()).commit();
 
     }
 
@@ -23,6 +25,19 @@ public class Preferences extends Activity {
 
             getPreferenceManager().setSharedPreferencesMode(MODE_WORLD_READABLE);
             addPreferencesFromResource(R.xml.prefs);
+            Preference pref = this.findPreference("pref_show_app_icon");
+            pref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    Activity act = getActivity();
+                    PackageManager p = act.getPackageManager();
+                    int state = (Boolean) newValue ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED : PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+                    final ComponentName alias = new ComponentName(getActivity(), "com.germainz.crappalinks.Preferences-Alias");
+                    p.setComponentEnabledSetting(alias, state, PackageManager.DONT_KILL_APP);
+                    return true;
+                }
+            });
         }
     }
 }
